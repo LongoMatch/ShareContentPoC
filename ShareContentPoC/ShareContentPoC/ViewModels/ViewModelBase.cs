16a865a81f6ae45eb3b3b4d380ff0888e5edc21f
Martin Vega <mvega@longomatch.com>
2024-01-12 12:02:20 +0100

    Add AvaloniaUI xplat app out of the box

---
+++ ShareContentPoC/ShareContentPoC/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace ShareContentPoC.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ ShareContentPoC/ShareContentPoC/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ShareContentPoC.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
